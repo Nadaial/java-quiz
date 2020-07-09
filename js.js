a65ae5ee3f93c2b4c795eb362bead708a8ad50ba
Nada Al <nadaabulibdeh@yahoo.ca>
2020-07-09 15:26:50 -0400

    added java.js updates

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 var startBtn = document.getElementById('start-btn');
+var nextBtn = document.getElementById('next-btn');
 var questionConEl=document.getElementById('question-con');
 var shuffleQue, currentQueIndex;
 var QueEl = document.getElementById('question');
@@ -24,17 +25,57 @@ nextQuestion()
 
 //Toset the next question 
 function nextQuestion(){
+    resetQue();
 showQue(shuffleQue[currentQueIndex])
 }
 
 function showQue(question){
     QueEl.innerText = question.question
+    question.answers.forEach(answer =>{
+        var button =document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct){
+            button.dataset.correct= answer.correct
+        }
+button.addEventListener('click', answer);
+answerBtnEl.appendChild(button);
+    }
+        )
+}
+
+function resetQue(){
+    nextBtn.classList.add('hide');
+    while (answerBtnEl.firstChild){
+        answerBtnEl.removeChild (answerBtnEl.firstChild);
+    }
+
 }
 
 //select the Answer function
 function answer(){
+var selectedBtn= e.target;
+var correct = selectedBtn.dataset.correct;
+setStatusClass(document.body, correct);
+Array.from (answerBtnEl.children).forEach(button => {
+    setStatusClass(button,button.dataset.correct);
 
+})
 }
+
+
+function setStatusClass(element, correct){
+    clearStatusClass (element);
+    if(correct){
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 
 var questions =[
     {
