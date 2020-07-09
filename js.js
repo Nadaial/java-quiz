84612d47ca214de163de2a64afc3dea4edecd3e3
Nada Al <nadaabulibdeh@yahoo.ca>
2020-07-09 14:33:35 -0400

    added java.sc

---
+++ js.js
@@ -0,0 +1,25 @@
+var startBtn = document.getElementById('start-btn');
+var questionConEl=document.getElementById('question-con')
+
+//start the quiz
+startBtn.addEventListener('click', startQuiz);
+function startQuiz(){
+console.log("started");
+startBtn.classList.add("hide");
+questionConEl.classList.remove('hide');
+}
+
+
+
+
+
+//Toset the next question 
+function nextQuestion(){
+
+}
+
+
+//select the Answer function
+function answer(){
+
+}
